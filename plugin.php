8b131d5f054d68cd00f4bbcce1865fbb19438e99
smeyer <smeyer.ilias@gmx.de>
2021-11-11 11:26:48 +0100

    r7 support

--- plugin.php
+++ plugin.php
@@ -3,11 +3,11 @@
 $id = "vedaudfclaiming";
 
 // code version; must be changed for all code changes
-$version = '6.1.1';
+$version = '7.2.1';
 
 // ilias min and max version; must always reflect the versions that should run with the plugin
-$ilias_min_version = '6.1';
-$ilias_max_version = '6.99';
+$ilias_min_version = '7.1';
+$ilias_max_version = '7.99';
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = 'Stefan Meyer';
