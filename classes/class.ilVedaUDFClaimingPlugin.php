cc07868b0e30b895fe235eca9549ee3c35f4be83
Christoph Ludolf <ludolf@leifos.de>
2024-04-11 15:20:29 +0200

    adds include of ilUserDefinedFields to allow for the use of constants defined outside of class scope

--- classes/class.ilVedaUDFClaimingPlugin.php
+++ classes/class.ilVedaUDFClaimingPlugin.php
@@ -1,6 +1,8 @@
 <?php
 /* Copyright (c) 1998-2009 ILIAS open source, Extended GPL, see docs/LICENSE */
 
+include_once "Services/User/classes/class.ilUserDefinedFields.php";
+
 /**
  * Class ilVedaUDFClaiming
  * @author Stefan Meyer <smeyer.ilias@gmx.de>
